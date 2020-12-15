13a354ea72e7e3a25b11be2c78389c44a0d09cc2
Matheus Rocha <matheus_rocha98@outlook.com>
2020-12-14 21:16:20 -0300

    populate SectionAboutProject

--- src/graphql/queries/getLandingPage.ts
+++ src/graphql/queries/getLandingPage.ts
@@ -21,10 +21,22 @@ export const GET_LANDING_PAGE = /* GraphQL */ `
     }
   }
 
+  fragment sectionAboutProject on LandingPage {
+    sectionAboutProject {
+      image {
+        url
+        alternativeText
+      }
+      title
+      description
+    }
+  }
+
   query GET_LANDING_PAGE {
     landingPage {
       ...logo
       ...header
+      ...sectionAboutProject
     }
   }
 `
